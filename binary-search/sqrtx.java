2a655e986fddb3198a664dde2e7d23db7874c777
Pratik P Borhade <74528299+PratikBorhade302@users.noreply.github.com>
2021-09-24 18:23:53 +0530

    Create sqrtx.java

---
+++ binary-search/sqrtx.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 0;
+        int end = x;
+        int mid = 0;
+        if(x == 0 || x ==1) return x;
+        while(start + 1 != end){
+            mid = start + (end - start)/2;
+            
+            if(mid > x/mid) end = mid;
+            else if(mid < x/mid) start = mid;
+            else return mid;
+        }
+        return start;
+    }
+}
